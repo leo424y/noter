d6bdae9dc24cee4d032108365bcb0b0843cda77b
leo424y <leo424y@gmail.com>
2019-02-19 11:07:46 +0800

    sign in path

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users,
-             path: '',
-             path_names: {
-               sign_in: 'login',
-               sign_out: 'logout',
-               registration: 'signup'
-             },
-             controllers: {
-               sessions: 'sessions',
-               registrations: 'registrations'
-             }  
+  devise_for :users
+            #  path: '',
+            #  path_names: {
+            #    sign_in: 'login',
+            #    sign_out: 'logout',
+            #    registration: 'signup'
+            #  },
+            #  controllers: {
+            #    sessions: 'sessions',
+            #    registrations: 'registrations'
+            #  }  
   root "phones#index"
   resources :phones
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
